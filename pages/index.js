7d384a881bf67a9bec21055100619269d2f87dd8
ico4e <70281513+ico4e@users.noreply.github.com>
2020-12-14 06:36:15 -0500

    ico4e-portfolio-code Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,204 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import styles from '../styles/Home.module.sass';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import ReactTooltip from 'react-tooltip';
+import {Animated} from 'react-animated-css';
+import Image from 'next/image';
+import ReactParticles from 'react-particles-js';
+import particlesConfig from '../utils/particlesjs-config';
 
 export default function Home() {
+  function Particles({children}) {
+    return (
+      <div style={{position: 'relative'}}>
+        <ReactParticles
+          params={particlesConfig}
+          style={{
+            position: 'absolute',
+            zIndex: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+          }}
+        />
+        {children && <div style={{position: 'relative'}}>{children}</div>}
+      </div>
+    );
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className='main'>
+      <Particles>
+        <div className={styles.portfolio}>
+          <div>
+            <Animated animationIn='bounceInLeft'>
+              <h1 className={styles.h1_title}>Web Developer</h1>
+            </Animated>
+            <Animated animationIn='bounceInDown'>
+              <h2 className={styles.h2_subtitle}>React Frontend</h2>
+            </Animated>
+            <Animated animationIn='bounceInRight'>
+              <div className='d-flex justify-content-center'>
+                <p className={styles.p_paragraph}>
+                  Design / <span className={styles.span}>Code</span>
+                </p>
+              </div>
+            </Animated>
+            <Animated animationIn='zoomInDown'>
+              <div
+                className='d-flex justify-content-center'
+                style={{
+                  opacity: 0.8,
+                }}
+              >
+                <Image
+                  src='/images/ico4e.png'
+                  alt='profile image'
+                  width='112'
+                  height='112'
+                  style={{
+                    width: '22%',
+                    opacity: '.8',
+                  }}
+                  className='rounded-circle img-fluid d-block shadow-sm '
+                />
+              </div>
+            </Animated>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <Animated animationIn='bounceInUp'>
+              <div className='d-flex justify-content-center mt-4'>
+                <Link href='/portfolio'>
+                  <a
+                    rel='dofollow'
+                    className='btn btn-primary btn-lg py-3 shadow-sm rounded'
+                  >
+                    View Portfolio
+                    <FontAwesomeIcon
+                      className='ml-2 primary-arrow-right'
+                      icon='chevron-right'
+                    />
+                  </a>
+                </Link>
+                <Link href='/contact'>
+                  <a
+                    rel='dofollow'
+                    className='btn btn-secondary btn-lg py-3 rounded'
+                  >
+                    Contact
+                    <FontAwesomeIcon
+                      className='ml-2 secondary-arrow-right'
+                      icon='chevron-right'
+                    />
+                  </a>
+                </Link>
+              </div>
+              <div className={styles.social_media}>
+                <a
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://www.facebook.com/ico4e'
+                  className={styles.box_icon_facebook}
+                  data-tip='ico4e Facebook'
+                >
+                  <FontAwesomeIcon
+                    className={styles.icon_facebook}
+                    icon={['fab', 'facebook-f']}
+                  />
+                </a>
+                <a
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://linkedin.com/in/ico4e'
+                  className={styles.box_icon_linkedin}
+                  data-tip='ico4e Linkedin'
+                >
+                  <FontAwesomeIcon
+                    className={styles.icon_linkedin}
+                    icon={['fab', 'linkedin-in']}
+                  />
+                </a>
+                <a
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href='https://github.com/ico4e'
+                  className={styles.box_icon_github}
+                  data-tip='ico4e Github'
+                >
+                  <FontAwesomeIcon
+                    className={styles.icon_github}
+                    icon={['fab', 'github-alt']}
+                  />
+                </a>
+                <ReactTooltip place='bottom' type='dark' effect='solid' />
+              </div>
+            </Animated>
+          </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <style jsx>{`
+             {
+              /* BTN PRIMARY */
+            }
+            .btn-primary {
+              font-family: 'Questrial', sans-serif;
+              letter-spacing: 0.8px;
+              font-size: 16px;
+              border-radius: 0;
+              margin: 0 8px;
+              background: #9270be;
+              border: 0.4px solid #9270be;
+            }
+            .btn-primary:hover {
+              background: #8465ac;
+              border: 0.4px solid #8465ac;
+              color: #fff;
+            }
+            .btn-primary:hover .primary-arrow-right {
+              animation: left-right 1s ease infinite;
+            }
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+             {
+              /* BTN SECONDARY */
+            }
+            .btn-secondary {
+              font-family: 'Questrial', sans-serif;
+              letter-spacing: 0.8px;
+              font-size: 16px;
+              border-radius: 0;
+              margin: 0 8px;
+              background: #9270be;
+              border: 0.4px solid #9270be;
+              background: transparent;
+              border: 0.4px solid #fff;
+            }
+            .btn-secondary:hover {
+              background: #fff;
+              border: 0.4px solid #fff;
+              color: #2f3640;
+            }
+            .btn-secondary:hover .secondary-arrow-right {
+              animation: left-right 1s ease infinite;
+              font-size: 200px;
+            }
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+             {
+              /* ANIMATIONS */
+            }
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            @keyframes left-right {
+              0% {
+                transform: translateX(6px);
+              }
+              25% {
+                transform: translateX(0px);
+              }
+              100% {
+                transform: translateX(6px);
+              }
+            }
+          `}</style>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </Particles>
     </div>
-  )
+  );
 }
---
+++ utils/fontawesome.js
@@ -0,0 +1,36 @@
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {
+  faArrowLeft,
+  faChevronRight,
+  faCode,
+  faComment,
+  faEnvelope,
+  faGraduationCap,
+  faPalette,
+  faPaperPlane,
+  faPhone,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebookF,
+  faLinkedinIn,
+  faGithubAlt,
+  faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons';
+
+export default library.add(
+  faFacebookF,
+  faLinkedinIn,
+  faGithubAlt,
+  faChevronRight,
+  faEnvelope,
+  faPhone,
+  faWhatsapp,
+  faPaperPlane,
+  faUser,
+  faComment,
+  faArrowLeft,
+  faPalette,
+  faCode,
+  faGraduationCap
+);
